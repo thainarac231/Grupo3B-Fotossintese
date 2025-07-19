070c1a78c4f4547611b8185cb7b4ccd5999471ea
thainara231 <thainarac231thg@gmail.com>
2025-07-19 15:51:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+
 window.addEventListener("load", () => {
-      const intro = document.getElementById("intro");
-      const content = document.getElementById("site");
+  const intro = document.getElementById("intro");
+  const site = document.getElementById("site");
 
-      setTimeout(() => {
-        intro.style.opacity = 0;
-        setTimeout(() => {
-          
-          intro.style.display = "none";
-
-          content.style.display = "block";
-        }, 1000);
-
-      }, 1000);
-    });
+  setTimeout(() => {
+    intro.style.opacity = 0;
+    setTimeout(() => {
+      intro.style.display = "none";
+      site.style.display = "block";
+    }, 1000); // espera 1 segundo para sumir totalmente
+  }, 2000); // mostra a intro por 2 segundos
+});
