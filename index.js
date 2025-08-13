d686912724cc3739169bfa388a7a5af88fc3e04f
Yasminemsnu <yasminemanuele309@gmail.com>
2025-08-13 19:43:08 -0300

    intro

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
 window.addEventListener("load", () => {
   const intro = document.getElementById("intro");
   const site = document.getElementById("site");
 
   setTimeout(() => {
-    intro.style.opacity = 0; // começa a sumir
+    intro.style.opacity = 0; // começa o fade-out
     setTimeout(() => {
-      intro.style.display = "none"; // remove
-      site.style.display = "block"; // mostra site
+      intro.style.display = "none"; // remove do DOM
+      site.style.display = "block"; // mostra o site
     }, 1000); // espera o fade-out terminar
-  }, 1000); // tempo que a intro fica visível (2s)
+  }, 2000); // tempo que a intro fica (2 segundos)
 });
